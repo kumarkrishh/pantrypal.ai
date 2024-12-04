7c59f8967b79ba4b2c91007dc5c7b3a81072a8e6
Krish Kumar <krishs.kumar14@gmail.com>
2024-12-03 16:34:08 -0800

    fixed npm run build error

--- app/api/deleteRecipe/route.ts
+++ app/api/deleteRecipe/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '../auth/[...nextauth]/auth.config';
 import { getClientPromise } from '@/lib/mongodb';
 
 export async function DELETE(request: Request) {
